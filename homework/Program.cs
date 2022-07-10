ec11af6f3a1b0a49216a2a4694700f4ff306825a
sergei sam <ssi1970@mail.ru>
2022-07-10 21:14:19 +0700

    Задача 34

---
+++ homework/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Homework_5
+{
+       public class task_34
+
+    // Задайте массив заполненный случайными положительными трёхзначными числами.
+    //  Напишите программу, которая покажет количество чётных чисел в массиве. 
+    {
+        static void Main(string[] args)
+        {
+            void FillArray(int[] number)
+            {
+                int length = number.Length;
+                int index = 0;
+                while (index < length)
+                {
+                    number[index] = new Random().Next(0, 999);
+                    index++;
+                }
+            }
+
+            void PrintArray(int[] mass)
+            {
+                int count = mass.Length;
+                int index = 0;
+                while (index < count)
+                {
+                    Console.Write($"{mass[index].ToString()} ");
+                    index++;
+                }
+            }
+
+            void PrintAmt(int[] coll)
+            {
+                int count = coll.Length;
+                int i = 0;
+                int Sum =0;
+                while (i < count)
+                {
+                    if (coll[i] % 2 == 0)
+                    {
+                        Sum = Sum + 1;
+                    }
+                    i++;
+                }
+                Console.Write("Колличество четных чисел в массиве: " + Sum);
+            }
+
+            int[] array = new int[10];
+            Console.Clear();
+            FillArray(array);
+            PrintArray(array);
+            Console.WriteLine();
+            PrintAmt(array);
+
+        }
+    }
+}
